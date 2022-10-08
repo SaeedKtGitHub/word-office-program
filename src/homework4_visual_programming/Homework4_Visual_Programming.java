de931d3218af58ee801fd8ebfc3e59ec63d8d83b
Saeed kt <saeedcb00@gmail.com.com>
2022-10-08 14:32:19 +0300

    first commit

---
+++ src/homework4_visual_programming/Homework4_Visual_Programming.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework4_visual_programming;
+
+/**
+ *
+ * @author DeLL
+ */
+public class Homework4_Visual_Programming {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
